53fccef4add553ac175aaab0ec80057109dbff8e
tgolnoft <tgolnoft@gmail.com>
2019-02-07 13:52:59 -0700

    uploading client and server examples

---
+++ src/server/simNetSer.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+const (
+	Port        = "8080"
+	Host        = "localhost"
+	MessageSize = 1024
+)
+
+func main() {
+	listener, err := net.Listen("tcp", Host+":"+Port)
+	if err != nil {
+		fmt.Println("Error in Main: net.Listen: " + err.Error())
+		os.Exit(1)
+	}
+	i := 0
+	for {
+		connection, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error in Main: listener.connect: " + err.Error())
+		}
+		go handleConnection(connection, i)
+		i++
+	}
+
+}
+
+func handleConnection(connection net.Conn, num int) {
+	fmt.Println("Connected to ", num)
+	defer func() {
+		connection.Close()
+		fmt.Println("Connection ", num, " closed")
+	}()
+	buffer := make([]byte, MessageSize)
+	numBytes, err := connection.Read(buffer)
+	if err != nil && err.Error() != "EOF" {
+		fmt.Println("Error in server:HandleConnection: " + err.Error())
+		return
+	}
+
+	fmt.Print(string(buffer[:numBytes]) + "\n")
+}
